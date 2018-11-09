df03903c0b7e1abc60beb6544f4d275efa87e90a
Isaac Cook <isaac@simpload.com>
2018-11-09 13:13:02 -0600

    Add a bunch of useful cli commands

--- Python/cli.py
+++ Python/cli.py
@@ -4,6 +4,8 @@ import click
 import sys
 import logging
 import rpcconnections
+from tabulate import tabulate
+
 
 @click.group()
 @click.option('--port', '-p', default=5658, help='bismuth api port')
@@ -15,14 +17,74 @@ def cli(ctx, port, host, verbose, raw):
     connection = rpcconnections.Connection((host, port), verbose=verbose, raw=raw)
     ctx.obj['connection'] = connection
 
+
 @cli.command()
 @click.pass_context
 def status(ctx):
     con = ctx.obj['connection']
-    status = con.command('statusjson')
-    pprint.pprint(status)
-    balances = con.command('api_listbalance', [ [status['address'], ], 0, True])
+    node_status = con.command('statusjson')
+    pprint.pprint(node_status)
+    balances = con.command('api_listbalance', [ [node_status['address'], ], 0, True])
     pprint.pprint(balances)
+
+
+@cli.command()
+@click.pass_context
+@click.argument('path', type=str)
+def new_wallet(ctx, path):
+    wallet = rpcconnections.Wallet.generate()
+    wallet.save(path)
+    print("New wallet generated: {}".format(wallet.address))
+
+
+@cli.command()
+@click.pass_context
+@click.argument('addresses', nargs=-1)
+def balance(ctx, addresses):
+    con = ctx.obj['connection']
+    pprint.pprint(con.command('api_listbalance', [ addresses, 0, True]))
+
+
+@cli.command()
+@click.pass_context
+@click.argument('address', type=str)
+@click.option('--lookback', '-l', default=500)
+def txs(ctx, address, lookback):
+    con = ctx.obj['connection']
+    since_block = max(0, con.height() - lookback)
+    res = con.command(
+        'api_getblocksincewhere', [since_block, "address = '{0}' or recipient = '{0}'".format(address)])
+    res = [
+        {'bh': r[0],
+         'timestamp': r[1],
+         'address': r[2],
+         'recipient': r[3],
+         'amount': r[4],
+         'signature': r[5],
+         'public_key': r[6],
+         'block_hash': r[7],
+         'fee': r[8],
+         'reward': r[9],
+         'operation': r[10],
+         'openfield': r[11]} for r in res]
+    for r in res:
+        r.pop('timestamp')
+        r.pop('public_key')
+        r.pop('block_hash')
+        r.pop('signature')
+    print(tabulate(res, headers='keys'))
+
+@cli.command()
+@click.pass_context
+@click.argument('wallet_path', type=str)
+@click.argument('amount', type=float)
+@click.argument('address', type=str)
+@click.option('--openfield', '-o', default='')
+def send(ctx, wallet_path, address, amount, openfield):
+    con = ctx.obj['connection']
+    wallet = rpcconnections.Wallet.load(wallet_path)
+    con.send(wallet, address, amount, openfield=openfield)
+
 
 @cli.command()
 @click.pass_context
@@ -35,7 +97,7 @@ def mine(ctx, count):
     print("Chain at height {}".format(orig_height))
     ret = con.command('regtest_generate', [count])
     assert ret == 'OK'
-    for i in range(300):
+    for _ in range(300):
         if (con.height() - orig_height) >= count:
             break
         time.sleep(0.1)
@@ -57,4 +119,3 @@ if __name__ == '__main__':
     root.addHandler(ch)
 
     cli(obj={})
-
